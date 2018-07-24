05ee2c68fb33fef6d08b19ada6d71bddf1e039e4
nicholaslgoodman <i@nicholasleegoodman.com>
2018-07-23 19:44:49 -0500

    added use cases for fewer arguments

--- swntool.sh
+++ swntool.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-
-ruby $1.rb $2 >> $3 
+if [ $# -eq 3 ]
+   then
+      ruby $1.rb $2 >> $3 
+elif [ $# -eq 2 ]
+   then
+      ruby $1.rb $2
+elif [ $# -eq 1 ]
+   then
+      ruby $1.rb
+fi
